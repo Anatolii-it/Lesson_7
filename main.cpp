8f9a15a64e5be084a1d12f88201f501528ee393d
Анатолій <t0663261690@gmail.com>
2024-03-27 14:26:17 +0200

    hw_1

--- main.cpp
+++ main.cpp
@@ -16,10 +16,10 @@ int main() {
     Complex result_mul = multiply(num1, num2);
     Complex result_div = divide(num1, num2);
 
-    cout << "    Сума: " << result_sum.real << " + " << result_sum.imag << "i" << endl;
-    cout << " Різниця: " << result_sub.real << " + " << result_sub.imag << "i" << endl;
-    cout << "Множення: " << result_mul.real << " + " << result_mul.imag << "i" << endl;
-    cout << " Ділення: " << result_div.real << " + " << result_div.imag << "i" << endl;
+    cout << "     Сума: " << result_sum.real << " + " << result_sum.imag << "i" << endl;
+    cout << "  Різниця: " << result_sub.real << " + " << result_sub.imag << "i" << endl;
+    cout << " Множення: " << result_mul.real << " + " << result_mul.imag << "i" << endl;
+    cout << "  Ділення: " << result_div.real << " + " << result_div.imag << "i" << endl;
     _getch();
     return 0;
 }
